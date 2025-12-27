e852a02ef36bb26c03b4ea78af32b07d049f64fb
thrilok752 <chotabheemlagudu@gmail.com>
2025-12-27 22:23:41 +0530

    Reset settings to SQLite for initial EC2 deployment

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-g#8u6v@7u61ehcygp)(^tl7a2bkhl5&202j&2)((q7%y3mg&5_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
@@ -79,22 +83,22 @@ CORS_ALLOWED_ORIGINS= ['http://localhost:5173']
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tododb',
-        'USER':'postgres',
-        'PASSWORD':'rollno21',
-        'HOST':'127.0.0.1',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tododb',
+#         'USER':'postgres',
+#         'PASSWORD':'rollno21',
+#         'HOST':'127.0.0.1',
+#         'PORT':'5432'
+#     }
+# }
 
 
 # Password validation
